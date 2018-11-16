6272284bdc12aeca09478d4f1a870bae3dcc9219
Ted Selegran <tedsele@lycksele.se>
2018-11-16 11:15:40 +0100

    Module things

--- projects/Javascript 101/assets/js/5-array.js
+++ projects/Javascript 101/assets/js/5-array.js
@@ -1,7 +1,40 @@
-alert("Hello")
 var friend1 = "Måns";
 var friend2 = "Sten";
 var friend3 = "Claes";
 var friend4 = "Måna";
 
-var friends = ["Måns", "Sten", "Claes", "Måna"];
+var friends = ["Måns", "Sten", "Claes", "Måns syster Måna"];
+
+console.log(friends[0] + " <3 " + friends[3]);
+
+friends[0] = "Göran";
+
+console.log(friends[0] + " <3 " + friends[3]);
+
+var colors = ["red", "green", "blue"];
+console.log(colors);
+
+colors.push("purple");
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+
+colors.unshift("Yellow");
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+console.log(colors.indexOf("green"))
+
+var fruits = ["Banana", "Orange", "Lemon", "Mango", "Pearl"]
+
+var citrus = fruits.slice([1], [3]);
+
+console.log(citrus);
+
+var cars = ["Audi A6", "Volvo 940", "Ford Mondeo", "Ferrari", "Landa", "Lada", "Citroên"];
+for(var i = 0; i < cars.length; i++) {
+    console.log(cars[i]);
+}
