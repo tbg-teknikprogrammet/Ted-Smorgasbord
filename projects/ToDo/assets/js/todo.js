9d4189391a57e17f77e73e5342ede7821bef6968
Ted Selegran <tedsele@lycksele.se>
2018-11-30 11:17:09 +0100

    Worked on toDo

--- projects/ToDo/assets/js/todo.js
+++ projects/ToDo/assets/js/todo.js
@@ -3,3 +3,21 @@
 // if()
 // for
 // array med tillhörande metod
+
+document.getElementById("button").addEventListener("click", toDo)
+
+function toDo(arr) {
+    net = [1,2,3]
+    var command = Array(prompt("Give a command"))
+    console.log(command)
+    if (command == "quit"){
+        alert("See ya!")
+    }
+    else if(command == "list"){
+        alert(net)
+    }
+    else if(command == "new"){
+        command = prompt("Vad vill du?")
+        net.push(command)
+    }
+}
