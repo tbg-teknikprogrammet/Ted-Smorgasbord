b771eee3ae6b33e7634e354674154e98db511917
Ted Selegran <tedsele@lycksele.se>
2018-12-06 13:59:43 +0100

    Fixade några fel.

--- projects/ToDo/assets/js/todo.js
+++ projects/ToDo/assets/js/todo.js
@@ -6,18 +6,26 @@
 
 document.getElementById("button").addEventListener("click", toDo)
 
-function toDo(arr) {
-    net = [1,2,3]
-    var command = Array(prompt("Give a command"))
-    console.log(command)
-    if (command == "quit"){
-        alert("See ya!")
+var net = []
+
+function toDo() {
+    var command = prompt("Please give a valid command");
+    while (command != "quit") {
+        console.log(command)
+        if (command == "del"){
+            console.log("");
+        }
+        else if(command == "list"){
+            console.log(net);
+            for(var i = 0; i < net.lenght, i++;) {
+                console.log("(" + i + ")" + command[i]);
+            }
+        }
+        else if(command == "new"){
+            command = prompt("Vad vill du?");
+            console.log("Saved" + command + ". Very nice")
+            net.push(command);
+        } // Command ska skrivas in efter
     }
-    else if(command == "list"){
-        alert(net)
-    }
-    else if(command == "new"){
-        command = prompt("Vad vill du?")
-        net.push(command)
-    }
+    console.log("See ya!")
 }
