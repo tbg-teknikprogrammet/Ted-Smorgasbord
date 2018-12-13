4b9bb046e5809edf69fb784df8a20b23ba88e300
Ted Selegran <tedsele@lycksele.se>
2018-12-13 02:20:41 +0100

    Did some adjustments and added some comments

--- projects/ToDo/assets/js/todo.js
+++ projects/ToDo/assets/js/todo.js
@@ -4,29 +4,29 @@
 // for
 // array med tillhörande metod
 
-document.getElementById("button").addEventListener("click", toDo)
+document.getElementById("button").addEventListener("click", toDo) // the toDo function starts when the button is pressed
 
-var net = []
+var net = [] // The list in question
 
-function toDo() {
-    while (command != "quit") {
+function toDo() { // When the button is pressed this function starts
+    while (command != "quit") { // You will be 'trapped' in this while loop until 'quit' is typed
         console.log(command)
-        if (command == "delete"){
+        if (command == "delete"){ // If delete is typed this will run
             command = prompt("Which index do you want to delete?")
-            net.splice(command , 1);
-            console.log("Deleted " + command);
+            net.splice(command , 1); // Depending what the user typed for index in the previous line, that index will be removed
+            console.log("Deleted " + command + ".");
         }
-        else if(command == "list"){
+        else if(command == "list"){ // If list is typed this will run
             for(var i = 0; i < net.length; i++) {
-                console.log("(" + i + ")" + net[i]);
+                console.log("(" + i + ")" + net[i]); // Types out in the console what index and what is typed in each index
             }
         }
-        else if(command == "new"){
-            command = prompt("Vad vill du?");
-            console.log("Saved " + command + ". Very nice")
-            net.push(command);
+        else if(command == "new"){ // If new is typed this will run
+            command = prompt("What do you want to add?");
+            console.log("Saved " + command + ".")
+            net.push(command); // Adds the input to the list in a new index
         }
-        var command = prompt("Please give a valid command");
+        var command = prompt("Please give a valid command.");
     }
-    console.log("See ya!") // So long space cowboy
+    console.log("Ending program!") // So long space cowboy
 }
