797ffe886388108af95af5399cac9e7cb19d56ed
Ted Selegran <tedsele@lycksele.se>
2019-03-20 11:22:10 +0100

    Fixed alot of things.

--- projects/js-playground/assets/js/js-playground.js
+++ projects/js-playground/assets/js/js-playground.js
@@ -1,3 +1,18 @@
-document.querySelector(".stepbro").addEventListener("click", function()
+// function openBox() {
+//     document.querySelector(".stepbro").addEventListener("click", function(){
+//         document.querySelector(".light").style.diplay = 'block';
+//     })
+//   }
 
-)
+//   openBox();
+
+// document.querySelectorAll(".light").style.diplay = "block";
+
+
+document.querySelector("#stepbro").addEventListener("click", function() {
+    document.querySelector("#light").style.visibility = "visible"
+})
+
+document.querySelector("#light").addEventListener("click", function() {
+    document.querySelector("#light").style.visibility = "hidden"
+})
