efb5f23ff9bf0b45e9d12301673088cb17b85891
Ted Selegran <tedsele@lycksele.se>
2019-03-27 11:15:50 +0100

    Worked on slideshow

--- projects/js-playground/assets/js/js-playground.js
+++ projects/js-playground/assets/js/js-playground.js
@@ -16,3 +16,25 @@ document.querySelector("#stepbro").addEventListener("click", function() {
 document.querySelector("#light").addEventListener("click", function() {
     document.querySelector("#light").style.visibility = "hidden"
 })
+
+var slidin = ["assets/images/Sneaky.jpg", "assets/images/rarara.jpg", "assets/images/hot.jpg"]
+
+var slideIndex = 0;
+
+document.querySelector("#slide").setAttribute("src", slidin[slideIndex])
+
+document.querySelector("#darnit").addEventListener("click", function() {
+    slideIndex += 1;
+    if(slideIndex > slidin.length-1) {
+        slideIndex = 0;
+    }
+    document.querySelector("#slide").setAttribute("src", slidin[slideIndex])
+})
+
+document.querySelector("#gosh").addEventListener("click", function() {
+    slideIndex += -1;
+    if(slideIndex < 0) {
+        slideIndex = slidin.length-1;
+    }
+    document.querySelector("#slide").setAttribute("src", slidin[slideIndex])
+})
