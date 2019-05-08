2bec01d7ba8bff5ce1dc4370e5c347893352490e
Ted Selegran <tedsele@lycksele.se>
2019-05-08 12:20:41 +0200

    fixed box 3 and started box 4

--- projects/js-playground/assets/js/js-playground.js
+++ projects/js-playground/assets/js/js-playground.js
@@ -8,6 +8,7 @@
 
 // document.querySelectorAll(".light").style.diplay = "block";
 
+document.querySelector("#stepbro").style.cursor = "pointer";
 
 document.querySelector("#stepbro").addEventListener("click", function() {
     document.querySelector("#light").style.visibility = "visible"
@@ -41,12 +42,33 @@ document.querySelector("#gosh").addEventListener("click", function() {
 
 document.addEventListener("keydown", function(event){
     if(event.which == 78) {
-        document.querySelector(".dang").style.background = "red";
+        document.querySelector(".box1").style.background = "red";
+        document.querySelector(".box2").style.background = "red";
+        document.querySelector(".box3").style.background = "red";
+        document.querySelector(".box4").style.background = "red";
+        document.querySelector(".box5").style.background = "red";
+        document.querySelector(".box6").style.background = "red";
     }
-    else if(event.which == 76) {
-        document.querySelector(".dang").style.background = "lime";
+    else if(event.which == 66) {
+        document.querySelector(".box1").style.background = "darkblue";
+        document.querySelector(".box2").style.background = "darkblue";
+        document.querySelector(".box3").style.background = "darkblue";
+        document.querySelector(".box4").style.background = "darkblue";
+        document.querySelector(".box5").style.background = "darkblue";
+        document.querySelector(".box6").style.background = "darkblue";
     }
     else if(event.which == 67) {
+        document.querySelector(".box1").style.background = "lime";
+        document.querySelector(".box2").style.background = "blue";
+        document.querySelector(".box3").style.background = "red";
+        document.querySelector(".box4").style.background = "white";
+        document.querySelector(".box5").style.background = "black";
+        document.querySelector(".box6").style.background = "yellow";
+    }
+    else if(event.which == 69) {
+    }
+    else if(event.which == 8) {
+        alert("anatawageidesu")
     }
     else {
         console.log("Oi")
